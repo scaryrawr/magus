a483a232ba4e781a3332e2e0b0c00f7f3971a0b0
Mike Wallio <michael.wallio@outlook.com>
2025-08-26 13:19:55 -0400

    Add switching tools capability

---
+++ apps/magus/src/contexts/ToolSetProvider.tsx
@@ -0,0 +1,44 @@
+import type { LanguageModel, ToolSet } from "ai";
+import { createContext, useContext, useEffect, useState, type ReactNode } from "react";
+import { useServerContext } from "./ServerProvider";
+
+type ToolSetInfo = {
+  tools: ToolSet | undefined;
+};
+
+type ToolSetProviderProps = {
+  children: ReactNode;
+  getToolSet: (model: LanguageModel) => ToolSet | undefined;
+};
+
+const ToolSetContext = createContext<ToolSetInfo>({
+  tools: undefined,
+});
+
+export const ToolSetProvider: React.FC<ToolSetProviderProps> = ({ children, getToolSet }) => {
+  const { state: serverState } = useServerContext();
+  const [model, setModel] = useState(() => serverState.model);
+  const [toolSet, setToolset] = useState(() => getToolSet(model));
+
+  useEffect(() => {
+    setToolset(getToolSet(model));
+  }, [model, getToolSet]);
+
+  useEffect(() => {
+    const handler = (model: LanguageModel) => {
+      setModel(model);
+    };
+
+    serverState.on("change:model", handler);
+    return () => {
+      serverState.off("change:model", handler);
+    };
+  }, [serverState]);
+
+  return <ToolSetContext.Provider value={{ tools: toolSet }}>{children}</ToolSetContext.Provider>;
+};
+
+export const useToolSetContext = (): ToolSetInfo => {
+  const context = useContext(ToolSetContext);
+  return context;
+};
--- apps/magus/src/main.tsx
+++ apps/magus/src/main.tsx
@@ -2,28 +2,24 @@ import { createLmStudioProvider, createOllamaProvider } from "@magus/providers";
 import { createServer } from "@magus/server";
 import {
   createCreateFileTool,
+  createEditorTool,
   createInsertTool,
   createShellTool,
   createStringReplaceTool,
   createViewTool,
 } from "@magus/tools";
+import type { LanguageModel, ToolSet } from "ai";
 import { render } from "ink";
 import { App } from "./app";
 import { ServerProvider } from "./contexts";
+import { ToolSetProvider } from "./contexts/ToolSetProvider";
 
 const createMagusServer = () => {
   const providers = [createLmStudioProvider(), createOllamaProvider()];
   const service = createServer({
     providers,
     model: providers[0].model("openai/gpt-oss-20b"),
-    tools: {
-      ...createShellTool(),
-      //...createEditorTool(),
-      ...createCreateFileTool(),
-      ...createInsertTool(),
-      ...createViewTool(),
-      ...createStringReplaceTool(),
-    },
+    tools: undefined,
   });
 
   return {
@@ -32,10 +28,49 @@ const createMagusServer = () => {
   };
 };
 
+const getProviderToolSet = (provider: string): ToolSet => {
+  switch (provider) {
+    case "lmstudio":
+      return {
+        ...createShellTool(),
+        ...createCreateFileTool(),
+        ...createInsertTool(),
+        ...createViewTool(),
+        ...createStringReplaceTool(),
+      };
+    default:
+      return {
+        ...createShellTool(),
+        ...createEditorTool(),
+      };
+  }
+};
+
+const getToolSet = (() => {
+  const cache = new Map<string, ToolSet>();
+  return (model: LanguageModel) => {
+    let cacheKey = "default";
+    if (typeof model !== "string") {
+      cacheKey = model.provider.replace(".chat", "");
+    }
+
+    let value = cache.get(cacheKey);
+    if (value) {
+      return value;
+    }
+
+    value = getProviderToolSet(cacheKey);
+    cache.set(cacheKey, value);
+    return value;
+  };
+})();
+
 try {
   await render(
     <ServerProvider createServer={createMagusServer}>
-      <App />
+      <ToolSetProvider getToolSet={getToolSet}>
+        <App />
+      </ToolSetProvider>
     </ServerProvider>,
   ).waitUntilExit();
 } catch (error) {

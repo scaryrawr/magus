5093441f84a773063d1b0df86062f7ecd781b533
Mike Wallio <michael.wallio@outlook.com>
2025-08-29 10:30:01 -0400

    Fix bundle/build scripts

--- apps/magus/bundle.ts
+++ apps/magus/bundle.ts
@@ -4,7 +4,7 @@ import { $ } from "bun";
 
 const targets = ["bun-linux-x64", "bun-linux-arm64", "bun-windows-x64", "bun-darwin-x64", "bun-darwin-arm64"];
 const buildTarget = async (target: string) => {
-  await $`bun build --compile --target=${target} --minify --sourcemap ./src/main.tsx --outfile dist/${target}/magus`;
+  await $`bun build --compile --target=${target} --minify --sourcemap ./src/main.ts --outfile dist/${target}/magus`;
 };
 
 const bundleTasks = targets.map(buildTarget);

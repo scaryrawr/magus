a0f1ad8802705956d11953b8917c9e2de8aaf76e
Mike Wallio <michael.wallio@outlook.com>
2025-08-29 14:46:00 -0400

    Add generic best effort tool call display

--- packages/react-tools/src/components/ToolView.tsx
+++ packages/react-tools/src/components/ToolView.tsx
@@ -1,4 +1,5 @@
-import { Box } from "ink";
+import type { ToolUIPart, UIDataTypes, UIMessagePart, UITools } from "ai";
+import { Box, Text } from "ink";
 import React, { useMemo } from "react";
 import {
   CreateFileView,
@@ -11,6 +12,20 @@ import {
 } from ".";
 import type { UIToolProps } from "./type";
 
+type ToolBoxProps = React.PropsWithChildren;
+
+const ToolBox: React.FC<ToolBoxProps> = ({ children }) => {
+  return (
+    <Box borderStyle="single" borderDimColor borderColor="yellow" flexDirection="column" width="80%" alignSelf="center">
+      {children}
+    </Box>
+  );
+};
+
+const isToolPart = (part: UIMessagePart<UIDataTypes, UITools>): part is ToolUIPart => {
+  return part.type.startsWith("tool-");
+};
+
 export const ToolView: React.FC<UIToolProps> = ({ part }) => {
   // Initialize the array inside the component to avoid circular dependency issues
   const toolViews = useMemo(
@@ -18,26 +33,56 @@ export const ToolView: React.FC<UIToolProps> = ({ part }) => {
     [],
   );
 
+  if (!isToolPart(part)) {
+    return null;
+  }
+
   for (const view of toolViews) {
     const element = view({ part });
     if (
       element !== null &&
       (React.isValidElement(element) || typeof element === "string" || typeof element === "number")
     ) {
-      return (
-        <Box
-          borderStyle="single"
-          borderDimColor
-          borderColor="yellow"
-          flexDirection="column"
-          width="80%"
-          alignSelf="center"
-        >
-          {element}
-        </Box>
-      );
+      return <ToolBox>{element}</ToolBox>;
     }
   }
 
-  return null;
+  const { input } = part;
+  let message: string = "";
+  if (input) {
+    switch (typeof input) {
+      case "string":
+        message = input;
+        break;
+      case "object":
+        message = Object.entries(input).reduce((msg, [key, value]) => {
+          let valueMsg: string = "";
+          switch (typeof value) {
+            case "string":
+            case "number":
+            case "boolean":
+              valueMsg = String(value);
+              break;
+            case "object":
+              if (Array.isArray(value)) {
+                valueMsg = value.join(", ");
+              }
+          }
+
+          return `${msg} --${key} "${valueMsg}"`;
+        }, "");
+        break;
+      default:
+        message = String(input);
+        break;
+    }
+  }
+
+  return (
+    <ToolBox>
+      <Text>
+        ⚒️ {part.type.replace("tool-", "")} {message}
+      </Text>
+    </ToolBox>
+  );
 };

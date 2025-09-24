16afc189dec2936a3007126e300e0f12cdcb628c
Mike Wallio <michael.wallio@outlook.com>
2025-09-24 09:27:10 -0400

    Add useEffect to limit re-running during render

--- packages/react/src/components/FigletText.tsx
+++ packages/react/src/components/FigletText.tsx
@@ -1,10 +1,19 @@
 import figlet, { type FigletOptions } from "figlet";
 import { Text } from "ink";
+import { useEffect, useState } from "react";
 
 export type FigletTextProps = FigletOptions & { fontData?: string; children: string };
 
 export const FigletText = ({ children, fontData, ...options }: FigletTextProps) => {
-  if (options.font && fontData) figlet.parseFont(options.font, fontData);
-  const result = figlet.textSync(children, options);
-  return <Text>{result}</Text>;
+  const [text, setText] = useState<string>("");
+
+  useEffect(() => {
+    if (options.font && fontData) {
+      figlet.parseFont(options.font, fontData);
+    }
+
+    setText(figlet.textSync(children, options));
+  }, [children, fontData, options]);
+
+  return <Text>{text}</Text>;
 };

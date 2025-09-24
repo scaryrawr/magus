aa78f66a017244affeab785a28914add1b445ab6
Mike Wallio <michael.wallio@outlook.com>
2025-09-24 09:29:36 -0400

    Remove `...` to avoid creating new objects

--- packages/react/src/components/FigletText.tsx
+++ packages/react/src/components/FigletText.tsx
@@ -4,16 +4,17 @@ import { useEffect, useState } from "react";
 
 export type FigletTextProps = FigletOptions & { fontData?: string; children: string };
 
-export const FigletText = ({ children, fontData, ...options }: FigletTextProps) => {
+export const FigletText = (props: FigletTextProps) => {
+  const { font, fontData, children } = props;
   const [text, setText] = useState<string>("");
 
   useEffect(() => {
-    if (options.font && fontData) {
-      figlet.parseFont(options.font, fontData);
+    if (font && fontData) {
+      figlet.parseFont(font, fontData);
     }
 
-    setText(figlet.textSync(children, options));
-  }, [children, fontData, options]);
+    setText(figlet.textSync(children, props));
+  }, [children, font, fontData, props]);
 
   return <Text>{text}</Text>;
 };
